f3e70a2911ab5dad12fe7e968ff8ab8b2c8d66aa
talimal <talimal@post.bgu.ac.il>
2021-03-23 14:46:53 +0200

    turned class to singletons

--- Backend/Service/trading_system.py
+++ Backend/Service/trading_system.py
@@ -2,11 +2,31 @@
 
 from Backend.Domain.TradingSystem import TradingSystemManager
 
-def register(username,password):
-    return TradingSystemManager.register(username=username,password=password)
+class TradingSystem(object):
 
-def login(username, password):
-    return TradingSystemManager.login(username=username, password=password)
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        """ Static access method. """
+        if TradingSystem.__instance == None:
+            TradingSystem()
+        return TradingSystem.__instance
+
+    def __init__(self):
+        """ Virtually private constructor. """
+        if TradingSystem.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            TradingSystem.__instance = self
+            self.trading_system_manager = TradingSystemManager.TradingSystemManager()
+
+
+    def register(self,username,password):
+        return self.trading_system_manager.register(username=username,password=password)
+
+    def login(self,username, password):
+        return self.trading_system_manager.login(username=username, password=password)
 
 
 
