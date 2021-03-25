2da38b042b06ebb37d3818d2f8be5b0860d4508f
Inon Katz <inonk@post.bgu.ac.il>
2021-03-25 18:34:45 +0200

    added success flag, generic response and primitive parsable

--- Backend/response.py
+++ Backend/response.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import TypeVar, Generic
 
 """
 Interface that every object we wish to refer to in Response must implement.
@@ -13,19 +14,40 @@ class Parsable(ABC):
 
 
 """
-Return object which contains message to print and object to inspect.
+In order to support primitive return objects, a primitive parsable wrapper is introduced
+"""
+
+
+class PrimitiveParsable(Parsable):
+
+    def __init__(self, value):
+        self.value = value
+
+    def parse(self):
+        return self.value
+
+
+"""
+Return object which contains message to print and object to inspect, and success flag.
 *Important* each class which self.object refers to must implement parse()
 """
 
 
-class Response:
+T = TypeVar('T', bound=Parsable)
 
-    def __init__(self, obj: Parsable = None, msg="Uninitialized"):
+
+class Response(Generic[T]):
+
+    def __init__(self, success: bool, obj: T = None, msg="Uninitialized"):
         self.msg = msg
         self.object = obj
+        self.success = success
 
     def parse(self):
         return self.object.parse()
 
     def get_msg(self):
         return self.msg
+
+    def succeeded(self):
+        return self.success
