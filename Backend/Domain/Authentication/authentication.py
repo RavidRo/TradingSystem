997be58ab38d7ca18ca9ceaca3557df335b22820
Inon Katz <48633378+inonkatz444@users.noreply.github.com>
2021-03-26 18:05:40 +0300

    Delete authentication.py

--- Backend/Domain/Authentication/authentication.py
+++
@@ -1,9 +0,0 @@
-class Authentication(object):
-    def __init__(self):
-        pass
-
-    def login(self, username, password):
-        pass
-
-    def register(self, username, password):
-        pass
