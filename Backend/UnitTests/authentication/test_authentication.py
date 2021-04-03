4bfa9c772740e0fb77f1ea5be7c2e4348a6d7f97
talimal <talimal@post.bgu.ac.il>
2021-04-03 17:59:08 +0300

    added authentication tests with DBMock

--- Backend/UnitTests/authentication/test_authentication.py
+++ Backend/UnitTests/authentication/test_authentication.py
@@ -1,29 +1,29 @@
 import pytest
-from Backend.Domain.Authentication.authentication import Authentication
-from Backend.response import Response
+from Backend.UnitTests.authentication import authentication
+
 
 def test_login_all_good():
-    auth = Authentication.get_instance()
+    auth = authentication.get_instance()
     response = auth.login(username='tali',password='puppy')
     assert response.succeeded(), response.get_msg == "login succeeded"
 
 def test_login_username_doesnt_exist():
-    auth = Authentication.get_instance()
+    auth = authentication.get_instance()
     response = auth.login(username='shahaf',password='sadna')
     assert not response.succeeded(), response.get_msg == "username doesn't exist in the system"
 
 def test_login_username_exists_password_incorrect():
-    auth = Authentication.get_instance()
+    auth = authentication.get_instance()
     response = auth.login(username='Inon',password='gguy')
     assert not response.succeeded(), response.get_msg == "password incorrect"
 
 def test_register_username_already_exists():
-    auth = Authentication.get_instance()
+    auth = authentication.get_instance()
     response = auth.login(username='Omer',password='coooool')
     assert not response.succeeded(), response.get_msg == "username already exists"
 
 def test_register_all_good():
-    auth = Authentication.get_instance()
+    auth = authentication.get_instance()
     response = auth.login(username='Shahaf',password='sadna')
     assert response.succeeded(), response.get_msg == "registration succeeded"
 
