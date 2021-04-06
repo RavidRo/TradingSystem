6fcf742c8e7c0d59ba6380cf07d7fc37d53f0e2d
Ravid Rom <ravidro@post.bgu.ac.il>
2021-04-06 09:41:36 +0300

    Made trading systme manager methods static

--- Backend/Domain/TradingSystem/trading_system_manager.py
+++ Backend/Domain/TradingSystem/trading_system_manager.py
@@ -11,60 +11,80 @@ from Backend.Domain.TradingSystem.responsibility_data import responsibility_data
 from Backend.Domain.TradingSystem.ITradingSystemManager import ITradingSystem
 from Backend.response import Response, ParsableList, PrimitiveParsable
 
-class TradingSystem(ITradingSystem):
+
+class TradingSystemManager(ITradingSystem):
 
     # 2.1
     # returns the guest newly created cookie
+    @staticmethod
     def enter_system() -> str:
         return UserManager.enter_system()
 
     # 2.3
+    @staticmethod
     def register(username: str, password: str, cookie: str) -> Response[None]:
         return UserManager.register(username, password, cookie)
 
     # 2.4
+    @staticmethod
     def login(username: str, password: str, cookie: str) -> Response[None]:
         return UserManager.login(username, password, cookie)
 
     # 2.5
+    @staticmethod
     def get_stores_details() -> Response[ParsableList[store_data]]:
         return StoresManager.get_stores_details().parse()
 
     # 2.5
+    @staticmethod
     def get_products_by_store(store_id: str) -> Response[ParsableList[product_data]]:
         return StoresManager.get_products_by_store(store_id).parse()
 
     # 2.6
     # kwargs = You can search for a product by additional key words
+    @staticmethod
     def search_products(
         product_name="", category=None, min_price=None, max_price=None, *keywords: tuple[str]
     ) -> Response[ParsableList[product_data]]:
-        return SearchEngine.search_products(product_name, category, min_price, max_price, *keywords).parse()
+        return SearchEngine.search_products(
+            product_name, category, min_price, max_price, *keywords
+        ).parse()
 
     # 2.7
-    def save_product_in_cart(cookie: str, store_id: str, product_id: str, quantity=1) -> Response[None]:
+    @staticmethod
+    def save_product_in_cart(
+        cookie: str, store_id: str, product_id: str, quantity=1
+    ) -> Response[None]:
         return UserManager.add_to_cart(cookie, store_id, product_id, quantity)
 
     # 2.8
+    @staticmethod
     def get_cart_details(cookie: str) -> Response[shopping_cart_data]:
         return UserManager.get_cart_details(cookie).parse()
 
     # 2.8
+    @staticmethod
     def remove_product_from_cart(cookie: str, product_id: str) -> Response[None]:
         return UserManager.remove_product_from_cart(cookie, product_id)
 
     # 2.8
-    def change_product_quantity_in_cart(self, store_id: str, product_id: str, new_quantity: int) -> Response[None]:
+    @staticmethod
+    def change_product_quantity_in_cart(
+        self, store_id: str, product_id: str, new_quantity: int
+    ) -> Response[None]:
         return UserManager.change_product_quantity_in_cart(store_id, product_id, new_quantity)
 
     # 2.9
+    @staticmethod
     def purchase_cart(cookie: str) -> Response[PrimitiveParsable[float]]:
         return UserManager.purchase_cart(cookie).parse()
 
     # 2.9
+    @staticmethod
     def purchase_completed(cookie: str) -> Response[None]:
         return UserManager.purchase_completed(cookie)
 
+    @staticmethod
     def get_cart_price(cookie: str) -> Response[PrimitiveParsable[float]]:
         return UserManager.get_cart_price(cookie)
 
@@ -72,6 +92,7 @@ class TradingSystem(ITradingSystem):
     # ===============================
 
     # 3.2
+    @staticmethod
     def create_store(cookie: str, name: str) -> Response[None]:
         Response = UserManager.create_store(cookie, name)
         if Response.success:
@@ -79,6 +100,7 @@ class TradingSystem(ITradingSystem):
         return Response
 
     # 3.7
+    @staticmethod
     def ger_purchase_history(cookie: str) -> Response[ParsableList[purchase_details_data]]:
         return UserManager.ger_purchase_history(cookie).parse()
 
@@ -87,66 +109,101 @@ class TradingSystem(ITradingSystem):
 
     # 4.1
     # Creating a new product a the store and setting its quantity to 0
-    def create_product(cookie: str, store_id: str, name: str, price: float, quantity: int) -> Response[None]:
+    @staticmethod
+    def create_product(
+        cookie: str, store_id: str, name: str, price: float, quantity: int
+    ) -> Response[None]:
         return UserManager.create_product(cookie, store_id, name, price, quantity)
 
     # 4.1
+    @staticmethod
     def remove_products(cookie: str, store_id: str, product_id: str) -> Response[None]:
         return UserManager.remove_products(cookie, store_id, product_id)
 
     # 4.1
-    def change_product_quantity(cookie: str, store_id: str, product_id: str, quantity: int) -> Response[None]:
+    @staticmethod
+    def change_product_quantity(
+        cookie: str, store_id: str, product_id: str, quantity: int
+    ) -> Response[None]:
         return UserManager.change_product_quantity(cookie, store_id, product_id, quantity)
 
     # 4.1
-    def edit_product_details(cookie: str, store_id: str, product_id: str, new_name: str, new_price: float) -> Response[None]:
+    @staticmethod
+    def edit_product_details(
+        cookie: str, store_id: str, product_id: str, new_name: str, new_price: float
+    ) -> Response[None]:
         return UserManager.set_product_price(cookie, store_id, product_id, new_name, new_price)
 
     # 4.3
+    @staticmethod
     def appoint_owner(cookie: str, store_id: str, username: str) -> Response[None]:
         return UserManager.appoint_owner(cookie, store_id, username)
 
     # 4.5
+    @staticmethod
     def appoint_manager(cookie: str, store_id: str, username: str) -> Response[None]:
         return UserManager.appoint_manager(cookie, store_id, username)
 
     # 4.6
-    def add_manager_permission(cookie: str, store_id: str, username: str, permission_number: int) -> Response[None]:
-        return UserManager.add_manager_permission(cookie, store_id, username, permissions(permission_number))
+    @staticmethod
+    def add_manager_permission(
+        cookie: str, store_id: str, username: str, permission_number: int
+    ) -> Response[None]:
+        return UserManager.add_manager_permission(
+            cookie, store_id, username, permissions(permission_number)
+        )
 
     # 4.6
-    def remove_manager_permission(cookie: str, store_id: str, username: str, permission_number: int) -> Response[None]:
-        return UserManager.remove_manager_permission(cookie, store_id, username, permissions(permission_number))
+    @staticmethod
+    def remove_manager_permission(
+        cookie: str, store_id: str, username: str, permission_number: int
+    ) -> Response[None]:
+        return UserManager.remove_manager_permission(
+            cookie, store_id, username, permissions(permission_number)
+        )
 
     # 4.4, 4.7
+    @staticmethod
     def remove_appointment(cookie: str, store_id: str, username: str) -> Response[None]:
         return UserManager.remove_appointment(cookie, store_id, username)
 
     # 4.9
+    @staticmethod
     def get_store_appointments(cookie: str, store_id: str) -> Response[responsibility_data]:
         return UserManager.get_store_appointments(cookie, store_id).parse()
 
     # 4.11
-    def get_store_purchases_history(cookie: str, store_id: str) -> Response[ParsableList[purchase_details_data]]:
+    @staticmethod
+    def get_store_purchases_history(
+        cookie: str, store_id: str
+    ) -> Response[ParsableList[purchase_details_data]]:
         return UserManager.get_store_purchases_history(cookie, store_id).parse()
 
     # System Manager
     # ====================
 
     # 6.4
-    def get_any_user_purchase_history(cookie: str, username: str) -> Response[ParsableList[purchase_details_data]]:
+    @staticmethod
+    def get_any_user_purchase_history(
+        cookie: str, username: str
+    ) -> Response[ParsableList[purchase_details_data]]:
         return UserManager.get_any_user_purchase_history(cookie, username).parse()
 
     # 6.4
-    def get_any_store_purchase_history(cookie: str, store_id: str) -> Response[ParsableList[purchase_details_data]]:
+    @staticmethod
+    def get_any_store_purchase_history(
+        cookie: str, store_id: str
+    ) -> Response[ParsableList[purchase_details_data]]:
         return UserManager.get_any_store_purchase_history(cookie, store_id).parse()
 
     # Inter component functions
     # ============================
     # 6.4
+    @staticmethod
     def get_any_user_purchase_history(username: str) -> Response[ParsableList[purchase_details]]:
         return UserManager.get_any_user_purchase_history(username).parse()
 
     # 6.4
+    @staticmethod
     def get_any_store_purchase_history(store_id: str) -> Response[ParsableList[purchase_details]]:
         return StoresManager.get_any_store_purchase_history(store_id).parse()
