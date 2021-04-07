0d9a37287ff2b2a61bb60578632b57dca6ba8fae
Ravid Rom <ravidro@post.bgu.ac.il>
2021-04-07 17:55:16 +0300

    Update test_authentication.py

--- Backend/UnitTests/authentication/test_authentication.py
+++ Backend/UnitTests/authentication/test_authentication.py
@@ -5,7 +5,6 @@ from Backend.UnitTests.authentication import authentication
 def test_login_all_good():
     auth = authentication.Authentication.get_instance()
     response = auth.login(username='Tali',password='puppy')
-    print(response.succeeded())
     assert response.succeeded(), response.get_msg == "login succeeded"
 
 def test_login_username_doesnt_exist():
@@ -27,6 +26,3 @@ def test_register_all_good():
     auth = authentication.Authentication.get_instance()
     response = auth.register(username='Shahaf',password='sadna')
     assert response.succeeded(), response.get_msg == "registration succeeded"
-
-# if __name__=="__main__":
-#     test_register_all_good()
