c783c6d5c6b8445b923cbc563007f45a78bf3af5
SeanPikulin <48625613+SeanPikulin@users.noreply.github.com>
2021-04-07 21:59:03 +0300

    Update Backend/Domain/TradingSystem/purchase_details.py
    Co-authored-by: Ravid Rom <ravidro@post.bgu.ac.il>

--- Backend/Domain/TradingSystem/purchase_details.py
+++ Backend/Domain/TradingSystem/purchase_details.py
@@ -6,11 +6,8 @@ from Backend.response import Response, Parsable
 
 @dataclass
 class PurchaseDetails(IPurchaseDetails):
-
-    def __init__(self, user_name: str, store_name: str, product_names: list, date: datetime, total_price: float):
-        self.user_name = user_name
-        self.product_names = product_names
-        self.store_name = store_name
-        self.date = date
-        self.total_price = total_price
-
+        user_name : str
+        store_name : str
+        product_names : list[str]
+        date : datetime
+        total_price : float
