ebf5326827ee2c6ba08d97b7a5ffcf7b4be2d9d5
SeanPikulin <pikulin@post.bgu.ac.il>
2021-04-08 22:32:44 +0300

    interface change

--- Backend/Domain/TradingSystem/Interfaces/IProduct.py
+++ Backend/Domain/TradingSystem/Interfaces/IProduct.py
@@ -6,7 +6,7 @@ from Backend.response import Parsable
 class IProduct(Parsable, metaclass=ABC):
 
     @abstractmethod
-    def __init__(self, product_name: str, price: float, qunatity: int):
+    def __init__(self, product_name: str, price: float):
         """Create a new store with it's specified info"""
         raise NotImplementedError
 
