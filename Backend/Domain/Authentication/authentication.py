bfe70005d96bd68243c5bcaaa8ee719d631350c2
Ravid Rom <ravidro@post.bgu.ac.il>
2021-04-09 18:53:19 +0300

    Fixed typo

--- Backend/Domain/Authentication/authentication.py
+++ Backend/Domain/Authentication/authentication.py
@@ -50,7 +50,7 @@ class Authentication(IAuthentication):
                 return response.Response[None](success=False, msg="password incorrect")
 
             else:
-                is_admin = self.db_handler.is_username_admin(udername=username)
+                is_admin = self.db_handler.is_username_admin(username=username)
                 return response.Response[None](
                     success=True, obj=response.PrimitiveParsable(is_admin), msg="login succeeded"
                 )
