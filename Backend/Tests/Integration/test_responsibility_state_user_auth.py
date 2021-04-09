a5338da642901cf89f27105d8fba4694ea9cec33
Ravid Rom <ravidro@post.bgu.ac.il>
2021-04-09 22:02:49 +0300

    fixed imports

--- Backend/Tests/Integration/test_responsibility_state_user_auth.py
+++ Backend/Tests/Integration/test_responsibility_state_user_auth.py
@@ -1,34 +1,34 @@
-# from Backend.Domain.TradingSystem.States.member import Member
-# from Backend.Domain.TradingSystem.States.admin import Admin
-# from Backend.Domain.TradingSystem.user import User
-# import pytest
+from Backend.Domain.TradingSystem.States.member import Member
+from Backend.Domain.TradingSystem.States.admin import Admin
+from Backend.Domain.TradingSystem.user import User
+import pytest
 
-# from Backend.Domain.TradingSystem.States.guest import Guest
-
-
-# @pytest.fixture
-# def user_guest():
-#     return User()
+from Backend.Domain.TradingSystem.States.guest import Guest
 
 
-# @pytest.fixture
-# def user_member():
-#     user = User()
-#     user.register("some_username", "password")
-#     user.login("some_username", "password")
-#     return user
+@pytest.fixture
+def user_guest():
+    return User()
 
 
-# @pytest.fixture
-# def user_admin():
-#     # ? @TODO create admin somehow
-#     user = User()
-#     user.register("some_username", "password")
-#     user.login("some_username", "password")
-#     return user
+@pytest.fixture
+def user_member():
+    user = User()
+    user.register("some_username", "password")
+    user.login("some_username", "password")
+    return user
 
 
-# # * get username
-# # * =================================================================
-# def test_guest_does_not_have_a_username(user_guest: User):
-#     assert not guest.get_username().succeeded()
+@pytest.fixture
+def user_admin():
+    # ? @TODO create admin somehow
+    user = User()
+    user.register("some_username", "password")
+    user.login("some_username", "password")
+    return user
+
+
+# * get username
+# * =================================================================
+def test_guest_does_not_have_a_username(user_guest: User):
+    assert not guest.get_username().succeeded()
