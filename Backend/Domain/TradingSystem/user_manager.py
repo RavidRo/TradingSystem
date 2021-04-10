ba7c4aa98ce1a726f9794c4b35cad015bd265256
Ravid Rom <ravidro@post.bgu.ac.il>
2021-04-10 15:59:42 +0300

    user manager refactoring

--- Backend/Domain/TradingSystem/user_manager.py
+++ Backend/Domain/TradingSystem/user_manager.py
@@ -1,13 +1,13 @@
-from Backend.Domain.TradingSystem.store import Store
 from typing import Callable
 import uuid
 
-from Backend.Domain.TradingSystem.Interfaces.IUser import IUser
-from .user import User
 from Backend.response import Response, ParsableList, PrimitiveParsable
+from Backend.Domain.TradingSystem.Interfaces.IUser import IUser
+from Backend.Domain.TradingSystem.store import Store
 from Backend.Domain.TradingSystem.shopping_cart import ShoppingCart
 from Backend.Domain.TradingSystem.purchase_details import PurchaseDetails
-from .Responsibilities.responsibility import Permission, Responsibility
+from Backend.Domain.TradingSystem.Responsibilities.responsibility import Permission, Responsibility
+from Backend.Domain.TradingSystem.user import User
 
 
 class UserManager:
@@ -48,31 +48,31 @@ class UserManager:
     # 2.3
     @staticmethod
     def register(username: str, password: str, cookie: str) -> Response[None]:
-        user = UserManager.__get_user_by_cookie(cookie)
-        if not user:
-            return Response(False, msg="No user is identified by the given cookie")
-        response = user.register(username, password)
-        if response.succeeded():
-            UserManager.username_user[username] = user
-        return response
+        def func(user: User):
+            response = user.register(username, password)
+            if response.succeeded():
+                UserManager.username_user[username] = user
+            return response
+
+        return UserManager.__deligate_to_user(cookie, func)
 
     # 2.4
     @staticmethod
     def login(username: str, password: str, cookie: str) -> Response[None]:
-        user = UserManager.__get_user_by_cookie(cookie)
-        if not user:
-            return Response(False, msg="No user is identified by the given cookie")
-        response = user.login(username, password)
+        def func(user: User):
+            response = user.login(username, password)
 
-        # If response succeeded we want to connect the cookie to the username
-        if response.succeeded():
-            for user_cookie in UserManager.cookie_user:
-                old_user = UserManager.cookie_user[user_cookie]
-                if old_user.get_username() == username and old_user != user:
-                    UserManager.cookie_user[cookie] = old_user
+            # If response succeeded we want to connect the cookie to the username
+            if response.succeeded():
+                for user_cookie in UserManager.cookie_user:
+                    old_user = UserManager.cookie_user[user_cookie]
+                    if old_user.get_username() == username and old_user != user:
+                        UserManager.cookie_user[cookie] = old_user
             # *This action will delete the current cart but will restore the old one and other user details
 
-        return response
+            return response
+
+        return UserManager.__deligate_to_user(cookie, func)
 
     # 2.7
     @staticmethod
