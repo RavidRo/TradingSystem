9413f2abe0a976ff6ab34559dca1fb7cdf690db1
Ravid Rom <ravidro@post.bgu.ac.il>
2021-04-10 14:40:52 +0300

    added @staticmethod

--- Backend/Domain/TradingSystem/user_manager.py
+++ Backend/Domain/TradingSystem/user_manager.py
@@ -13,33 +13,39 @@ class UserManager:
     cookie_user: dict[str, IUser] = {}
     username_user: dict[str, IUser] = {}
 
+    @staticmethod
     def __deligate_to_user(cookie, func):
         user = UserManager.__get_user_by_cookie(cookie)
         if not user:
             return Response(False, msg="No user is identified by the given cookie")
         return func(user)
 
+    @staticmethod
     def __get_user_by_cookie(cookie) -> IUser:
         if cookie not in UserManager.cookie_user:
             return None
         return UserManager.cookie_user[cookie]
 
+    @staticmethod
     def __get_user_by_username(username) -> IUser:
         if username not in UserManager.username_user:
             return None
         return UserManager.username_user[username]
 
+    @staticmethod
     def __create_cookie() -> str:
         return str(uuid.uuid4())
 
     # 2.1
     # returns the guest newly created cookie
+    @staticmethod
     def enter_system() -> str:
         cookie = UserManager.__create_cookie()
         UserManager.cookie_user[cookie] = IUser.create_user()
         return cookie
 
     # 2.3
+    @staticmethod
     def register(username: str, password: str, cookie: str) -> Response[None]:
         user = UserManager.__get_user_by_cookie(cookie)
         if not user:
@@ -50,6 +56,7 @@ class UserManager:
         return response
 
     # 2.4
+    @staticmethod
     def login(username: str, password: str, cookie: str) -> Response[None]:
         user = UserManager.__get_user_by_cookie(cookie)
         if not user:
@@ -67,6 +74,7 @@ class UserManager:
         return response
 
     # 2.7
+    @staticmethod
     def add_to_cart(cookie: str, store_id: str, product_id: str, quantity: int) -> Response[None]:
         func: Callable[[User], Response] = lambda user: user.add_to_cart(
             store_id, product_id, quantity
@@ -74,11 +82,13 @@ class UserManager:
         return UserManager.__deligate_to_user(cookie, func)
 
     # 2.8
+    @staticmethod
     def get_cart_details(cookie: str) -> Response[ShoppingCart]:
         func: Callable[[User], Response] = lambda user: user.get_cart_details()
         return UserManager.__deligate_to_user(cookie, func)
 
     # 2.8
+    @staticmethod
     def remove_product_from_cart(cookie: str, store_id: str, product_id: str) -> Response[None]:
         func: Callable[[User], Response] = lambda user: user.remove_product_from_cart(
             store_id, product_id
@@ -86,6 +96,7 @@ class UserManager:
         return UserManager.__deligate_to_user(cookie, func)
 
     # 2.8
+    @staticmethod
     def change_product_quantity_in_cart(
         cookie: str, store_id, product_id, new_amount
     ) -> Response[None]:
@@ -95,16 +106,19 @@ class UserManager:
         return UserManager.__deligate_to_user(cookie, func)
 
     # 2.9
+    @staticmethod
     def purchase_cart(cookie: str) -> Response[PrimitiveParsable[float]]:
         func: Callable[[User], Response] = lambda user: user.purchase_cart()
         return UserManager.__deligate_to_user(cookie, func)
 
     # 2.9
+    @staticmethod
     def purchase_completed(cookie: str) -> Response[None]:
         func: Callable[[User], Response] = lambda user: user.purchase_completed()
         return UserManager.__deligate_to_user(cookie, func)
 
     # 2.9
+    @staticmethod
     def get_cart_price(cookie: str) -> Response[PrimitiveParsable[float]]:
         func: Callable[[User], Response] = lambda user: user.get_cart_price()
         return UserManager.__deligate_to_user(cookie, func)
@@ -113,11 +127,13 @@ class UserManager:
     # ===============================
 
     # 3.2
+    @staticmethod
     def create_store(cookie: str, name: str) -> Response[None]:
         func: Callable[[User], Response] = lambda user: user.create_store(name)
         return UserManager.__deligate_to_user(cookie, func)
 
     # 3.7
+    @staticmethod
     def get_purchase_history(cookie: str) -> Response[ParsableList[PurchaseDetails]]:
         func: Callable[[User], Response] = lambda user: user.get_purchase_history()
         return UserManager.__deligate_to_user(cookie, func)
@@ -127,6 +143,7 @@ class UserManager:
 
     # 4.1
     # Creating a new product a the store and setting its quantity to 0
+    @staticmethod
     def create_product(
         cookie: str, store_id: str, name: str, price: float, quantity: int
     ) -> Response[None]:
@@ -136,6 +153,7 @@ class UserManager:
         return UserManager.__deligate_to_user(cookie, func)
 
     # 4.1
+    @staticmethod
     def remove_product_from_store(cookie: str, store_id: str, product_id: str) -> Response[None]:
         func: Callable[[User], Response] = lambda user: user.remove_product_from_store(
             store_id, product_id
@@ -143,6 +161,7 @@ class UserManager:
         return UserManager.__deligate_to_user(cookie, func)
 
     # 4.1
+    @staticmethod
     def change_product_quantity_in_store(
         cookie: str, store_id: str, product_id: str, quantity: int
     ) -> Response[None]:
@@ -152,6 +171,7 @@ class UserManager:
         return UserManager.__deligate_to_user(cookie, func)
 
     # 4.1
+    @staticmethod
     def edit_product_details(
         cookie: str, store_id: str, product_id: str, new_name: str, new_price: float
     ) -> Response[None]:
@@ -161,6 +181,7 @@ class UserManager:
         return UserManager.__deligate_to_user(cookie, func)
 
     # 4.3
+    @staticmethod
     def appoint_owner(cookie: str, store_id: str, username: str) -> Response[None]:
         user = UserManager.__get_user_by_username(username)
         if not user:
@@ -169,6 +190,7 @@ class UserManager:
         return UserManager.__deligate_to_user(cookie, func)
 
     # 4.5
+    @staticmethod
     def appoint_manager(cookie: str, store_id: str, username: str) -> Response[None]:
         user = UserManager.__get_user_by_username(username)
         if not user:
@@ -177,6 +199,7 @@ class UserManager:
         return UserManager.__deligate_to_user(cookie, func)
 
     # 4.6
+    @staticmethod
     def add_manager_permission(
         cookie: str, store_id: str, username: str, permission: Permission
     ) -> Response[None]:
@@ -186,6 +209,7 @@ class UserManager:
         return UserManager.__deligate_to_user(cookie, func)
 
     # 4.6
+    @staticmethod
     def remove_manager_permission(
         cookie: str, store_id: str, username: str, permission: Permission
     ) -> Response[None]:
@@ -193,16 +217,19 @@ class UserManager:
         return UserManager.__deligate_to_user(cookie, func)
 
     # 4.4, 4.7
+    @staticmethod
     def remove_appointment(cookie: str, store_id: str, username: str) -> Response[None]:
         func: Callable[[User], Response] = lambda user: user.remove_appointment(store_id, username)
         return UserManager.__deligate_to_user(cookie, func)
 
     # 4.9
+    @staticmethod
     def get_store_appointments(cookie: str, store_id: str) -> Response[Responsibility]:
         func: Callable[[User], Response] = lambda user: user.get_store_appointments(store_id)
         return UserManager.__deligate_to_user(cookie, func)
 
     # 4.11
+    @staticmethod
     def get_store_purchase_history(
         cookie: str, store_id: str
     ) -> Response[ParsableList[PurchaseDetails]]:
@@ -213,6 +240,7 @@ class UserManager:
     # ====================
 
     # 6.4
+    @staticmethod
     def get_any_store_purchase_history_admin(
         cookie: str, store_id: str
     ) -> Response[ParsableList[PurchaseDetails]]:
@@ -222,6 +250,7 @@ class UserManager:
         return UserManager.__deligate_to_user(cookie, func)
 
     # 6.4
+    @staticmethod
     def get_any_user_purchase_history_admin(
         cookie: str, username: str
     ) -> Response[ParsableList[PurchaseDetails]]:
@@ -234,5 +263,6 @@ class UserManager:
     # ====================
 
     # 6.4
+    @staticmethod
     def get_any_user_purchase_history(username: str) -> Response[ParsableList[PurchaseDetails]]:
         return UserManager.__get_user_by_username(username).get_purchase_history()
