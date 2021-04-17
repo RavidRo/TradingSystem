3793f5c3e7beb45a65235e4f464ff07deed2d56f
Inon Katz <inonk@post.bgu.ac.il>
2021-04-17 12:28:54 +0300

    Authentication is now static module, admin check is now Guest's responsibility

--- Backend/Tests/Unit/test_authentication.py
+++ Backend/Tests/Unit/test_authentication.py
@@ -1,12 +1,7 @@
 import pytest
 import json
 
-from Backend.Domain.Authentication.authentication import Authentication
-
-
-@pytest.fixture
-def auth():
-    return Authentication.get_instance()
+from Backend.Domain.Authentication import authentication
 
 
 @pytest.fixture
@@ -28,44 +23,29 @@ def admin_password(config_path):
         return data["admin-password"]
 
 
-def test_login_all_good(auth):
-    auth.register("test_login_all_good", "test_login_all_good")
-    response = auth.login("test_login_all_good", "test_login_all_good")
+def test_login_all_good():
+    authentication.register("test_login_all_good", "test_login_all_good")
+    response = authentication.login("test_login_all_good", "test_login_all_good")
     assert response.succeeded(), response.get_msg()
 
 
-def test_login_username_does_not_exist(auth):
-    response = auth.login(username="shahaf", password="sadna")
+def test_login_username_does_not_exist():
+    response = authentication.login(username="shahaf", password="sadna")
     assert not response.succeeded(), response.get_msg()
 
 
-def test_login_username_exists_password_incorrect(auth):
-    auth.register("some_username", "some_password")
-    response = auth.login(username="some_username", password="wrong_password")
+def test_login_username_exists_password_incorrect():
+    authentication.register("some_username", "some_password")
+    response = authentication.login(username="some_username", password="wrong_password")
     assert not response.succeeded(), response.get_msg()
 
 
-def test_login_as_admin_returns_true(auth: Authentication, admin_username, admin_password):
-    response = auth.login(admin_username, admin_password)
-    return response.succeeded() and response.get_obj().get_val(), response.get_msg()
-
-
-def test_login_as_none_admin_returns_false(auth: Authentication):
-    auth.register(
-        "test_login_as_none_admin_returns_false", "test_login_as_none_admin_returns_false"
-    )
-    response = auth.login(
-        "test_login_as_none_admin_returns_false", "test_login_as_none_admin_returns_false"
-    )
-    assert response.succeeded() and not response.get_obj().get_val(), response.get_msg()
-
-
-def test_register_username_already_exists(auth):
-    auth.register("some_username", "some_password")
-    response = auth.register(username="some_username", password="coooool")
+def test_register_username_already_exists():
+    authentication.register("some_username", "some_password")
+    response = authentication.register(username="some_username", password="coooool")
     assert not response.succeeded(), response.get_msg()
 
 
-def test_register_all_good(auth):
-    response = auth.register(username="to_register", password="sadna")
+def test_register_all_good():
+    response = authentication.register(username="to_register", password="sadna")
     assert response.succeeded(), response.get_msg()
