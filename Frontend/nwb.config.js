04ad25a6b84c893b122b8716d270ae8f46ac3f89
Ravid Rom <ravidro@post.bgu.ac.il>
2021-04-19 11:28:49 +0300

    first deployment to heroku

---
+++ Frontend/nwb.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	type: 'react-app',
+	devServer: {
+		proxy: {
+			'/': {
+				target: 'http://localhost:5000',
+				// target: 'https://trading-system-workshop.herokuapp.com/',
+				// pathRewrite: { '^/api': '' },
+			},
+		},
+	},
+};
