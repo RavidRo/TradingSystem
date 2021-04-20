afe07f4000eb1bd7d1f8f16b3a9e50d302990c71
Ravid Rom <ravidro@post.bgu.ac.il>
2021-04-20 13:35:09 +0300

    Automatic production variable in heroku

--- Frontend/nwb.config.js
+++ Frontend/nwb.config.js
@@ -3,7 +3,7 @@ module.exports = {
 	devServer: {
 		proxy: {
 			'/': {
-				target: require('../config.json').production
+				target: process.env.PRODUCTION
 					? 'https://trading-system-workshop.herokuapp.com/'
 					: 'http://localhost:5000',
 				// pathRewrite: { '^/api': '' },
