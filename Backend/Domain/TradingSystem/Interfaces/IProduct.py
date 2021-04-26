38ff840a2938854e910b1e1646cd23fd733d4967
Inon Katz <inonk@post.bgu.ac.il>
2021-04-26 18:29:45 +0300

    Fix stuff due to review

--- Backend/Domain/TradingSystem/Interfaces/IProduct.py
+++ Backend/Domain/TradingSystem/Interfaces/IProduct.py
@@ -5,7 +5,7 @@ from Backend.response import Parsable
 
 class IProduct(Parsable):
     @abstractmethod
-    def __init__(self, product_name: str, category, price: float):
+    def __init__(self, product_name: str, category: str, price: float):
         """Create a new store with it's specified info"""
         raise NotImplementedError
 
