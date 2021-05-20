ba8d8665d0ab791bf0aa87c97aa09dadb4f6d82f
Inon Katz <inonk@post.bgu.ac.il>
2021-05-20 23:58:43 +0300

    remove scoped_session, complete ProductHandler (basic testing), add save_bag (basic testing)

--- Backend/DataBase/database.py
+++ Backend/DataBase/database.py
@@ -1,17 +1,15 @@
 from datetime import date
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base, scoped_session
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 # create an engine
 
-SQLAlchemy_DATABASE_URI = 'postgresql://nklfongbputsdr' \
-                          ':780ea33e04aa8fb36400cd72967b97385d186978a31d262416b53791ae93c425@ec2-54-155-35-88.eu-west' \
-                          '-1.compute.amazonaws.com:5432/d58n31ogjjv4sj '
+SQLAlchemy_DATABASE_URI = 'postgresql://nklfongbputsdr:780ea33e04aa8fb36400cd72967b97385d186978a31d262416b53791ae93c425@ec2-54-155-35-88.eu-west-1.compute.amazonaws.com:5432/d58n31ogjjv4sj'
 engine = create_engine(SQLAlchemy_DATABASE_URI)
 
 # create a configured "Session" class
-Session = scoped_session(sessionmaker(bind=engine, autocommit=False, autoflush=False))
+Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 """:type: sqlalchemy.orm.scoped_session"""
 
 Base = declarative_base()
