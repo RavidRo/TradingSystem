29b8581e5f1ca7312cf275d73076129dfb79beab
Ravid Rom <ravidro@post.bgu.ac.il>
2021-06-09 18:00:47 +0300

    fixed enter system in api

--- Frontend/src/pages/Notifications.tsx
+++ Frontend/src/pages/Notifications.tsx
@@ -1,40 +1,40 @@
 import { GridList, GridListTile } from '@material-ui/core';
-import React, { FC,useState,useEffect} from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import Box from '@material-ui/core/Box';
-import {faEnvelopeOpenText} from '@fortawesome/free-solid-svg-icons';
+import { faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../styles/Notifications.scss';
-import {notificationTime } from '../types';
-import updateNotifications from '../components/Navbar';
-
+import { notificationTime } from '../types';
 
 type NotificationProps = {
-    location: any,
-    initializeNotifications: ()=>void,
+	location: any;
+	initializeNotifications: () => void;
 };
 
-const Notifications: FC<NotificationProps> = ({location, initializeNotifications}) => {
-    const [notifications, setNotifications] = useState<notificationTime>(location.state.notifications);
+const Notifications: FC<NotificationProps> = ({ location, initializeNotifications }) => {
+	const [notifications, setNotifications] = useState<notificationTime>(
+		location.state.notifications
+	);
 
-    useEffect(()=>{
-        setNotifications(location.state.notifications);
-        initializeNotifications();
-    },[]);
+	useEffect(() => {
+		setNotifications(location.state.notifications);
+		initializeNotifications();
+	}, []);
 
 	return (
-        <div className="NotificationsDiv" {...console.log(notifications)}>
-            <GridList cellHeight={130} cols={1}>
-                {notifications.map((_, index) => (
-                    <Box className="box" color="black" bgcolor="#fbd1b7" m={1}>
-                        <FontAwesomeIcon className="messageIcon" icon={faEnvelopeOpenText} />
-                        <GridListTile key={index}>
-                            <h3 className="notify">{notifications[index][0]}</h3>
-                            <h3 className="date">{notifications[index][1]}</h3>
-                        </GridListTile>
-                    </Box>
-                ))}
-            </GridList>
-        </div>
+		<div className='NotificationsDiv' {...console.log(notifications)}>
+			<GridList cellHeight={130} cols={1}>
+				{notifications.map((_, index) => (
+					<Box className='box' color='black' bgcolor='#fbd1b7' m={1}>
+						<FontAwesomeIcon className='messageIcon' icon={faEnvelopeOpenText} />
+						<GridListTile key={index}>
+							<h3 className='notify'>{notifications[index][0]}</h3>
+							<h3 className='date'>{notifications[index][1]}</h3>
+						</GridListTile>
+					</Box>
+				))}
+			</GridList>
+		</div>
 	);
 };
 
