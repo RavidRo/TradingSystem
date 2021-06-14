5e4d7bad1d2b025d256f85e14b044b5a2885ee81
Inon Katz <inonk@post.bgu.ac.il>
2021-06-14 20:20:12 +0300

    pre-merge commit

--- Backend/DataBase/Handlers/temp_main.py
+++ Backend/DataBase/Handlers/temp_main.py
@@ -14,126 +14,109 @@ from Backend.Service.trading_system import TradingSystem
 
 
 def save():
-    # mapper_registry.metadata.drop_all(engine)
-    # mapper_registry.metadata.create_all(engine)
-    # trading_system = TradingSystem.getInstance()
-    # cookie = trading_system.enter_system()
-    # res = trading_system.register(cookie, "user", "password")
-    # if not res.succeeded():
-    #     print("register: " + res.get_msg())
-    # res = trading_system.login(cookie, "user", "password")
-    # cookie2 = trading_system.enter_system()
-    # res2 = trading_system.register(cookie2, "user2", "password2")
-    # if not res2.succeeded():
-    #     print("register2: " + res2.get_msg())
-    #
-    # cookie3 = trading_system.enter_system()
-    # res3 = trading_system.register(cookie3, "user3", "password3")
-    # if not res3.succeeded():
-    #     print("register3: " + res3.get_msg())
-    #
-    # res2 = trading_system.login(cookie2, "user2", "password2")
-    # res3 = trading_system.login(cookie3, "user3", "password3")
-    #
-    # cookie4 = trading_system.enter_system()
-    # res4 = trading_system.register(cookie4, "user4", "password4")
-    # if not res4.succeeded():
-    #     print("register4: " + res4.get_msg())
-    #
-    # store_res = trading_system.create_store(cookie, "The Store")
-    # if not store_res.succeeded():
-    #     print("open_store: " + store_res.get_msg())
-    #
-    # store_res2 = trading_system.create_store(cookie2, "The Store of cookie2")
-    # if not store_res2.succeeded():
-    #     print("open_store: " + store_res2.get_msg())
-    #
-    # product_res = trading_system.create_product(cookie, store_res.get_obj(), "The Product", "The Category", 5, 8,
-    #                                             ["The Keyword A", "The Keyword B"])
-    # if not product_res.succeeded():
-    #     print("add_product: " + product_res.get_msg())
-    #
-    # product2_res = trading_system.create_product(cookie, store_res.get_obj(), "The Product2", "The Category2", 10, 16,
-    #                                              ["The Keyword A2", "The Keyword B2"])
-    # if not product2_res.succeeded():
-    #     print("add_product: " + product2_res.get_msg())
-    #
-    # res_save_prod = trading_system.save_product_in_cart(cookie2, store_res.get_obj(), product_res.get_obj(), 2)
-    # if not res_save_prod.succeeded():
-    #     print("save in cart: " + res_save_prod.get_msg())
-    #
-    # res_save_prod2 = trading_system.save_product_in_cart(cookie2, store_res.get_obj(), product2_res.get_obj(), 5)
-    # if not res_save_prod2.succeeded():
-    #     print("save in cart: " + res_save_prod.get_msg())
-    #
-    # res_create_offer = trading_system.create_offer(cookie2, store_res.get_obj(), product2_res.get_obj())
-    # if not res_create_offer.succeeded():
-    #     print("create offer: " + res_create_offer.get_msg())
-    #
-    # res_declare_price = trading_system.declare_price(cookie2, res_create_offer.get_obj(), 1.0)
-    # if not res_declare_price.succeeded():
-    #     print("create offer: " + res_declare_price.get_msg())
-    #
-    # res_appoint_owner = trading_system.appoint_owner(cookie, store_res.get_obj(), "user2")
-    # if not res_appoint_owner.succeeded():
-    #     print("appoint owner: " + res_appoint_owner.get_msg())
-    #
-    # res_appoint_owner = trading_system.appoint_owner(cookie2, store_res.get_obj(), "user3")
-    # if not res_appoint_owner.succeeded():
-    #     print("appoint owner: " + res_appoint_owner.get_msg())
-    #
-    # res_appoint_owner = trading_system.appoint_owner(cookie2, store_res2.get_obj(), "user")
-    # if not res_appoint_owner.succeeded():
-    #     print("appoint owner: " + res_appoint_owner.get_msg())
-    #
-    # res_appoint_manager = trading_system.appoint_manager(cookie, store_res2.get_obj(), "user4")
-    # if not res_appoint_manager.succeeded():
-    #     print("appoint manager: " + res_appoint_manager.get_msg())
-    #
-    # res_add_perm = trading_system.add_manager_permission(cookie, store_res2.get_obj(), "user4", "appoint manager")
-    # if not res_add_perm.succeeded():
-    #     print("add permission: " + res_add_perm.get_msg())
-    #
-    # res_remove_perm = trading_system.remove_manager_permission(cookie, store_res2.get_obj(), "user4", "get appointments")
-    # if not res_remove_perm.succeeded():
-    #     print("remove permission: " + res_remove_perm.get_msg())
+    mapper_registry.metadata.drop_all(engine)
+    mapper_registry.metadata.create_all(engine)
+    trading_system = TradingSystem.getInstance()
+    cookie = trading_system.enter_system()
+    res = trading_system.register(cookie, "user", "password")
+    if not res.succeeded():
+        print("register: " + res.get_msg())
+    res = trading_system.login(cookie, "user", "password")
+    cookie2 = trading_system.enter_system()
+    res2 = trading_system.register(cookie2, "user2", "password2")
+    if not res2.succeeded():
+        print("register2: " + res2.get_msg())
+
+    cookie3 = trading_system.enter_system()
+    res3 = trading_system.register(cookie3, "user3", "password3")
+    if not res3.succeeded():
+        print("register3: " + res3.get_msg())
+
+    res2 = trading_system.login(cookie2, "user2", "password2")
+    res3 = trading_system.login(cookie3, "user3", "password3")
+
+    cookie4 = trading_system.enter_system()
+    res4 = trading_system.register(cookie4, "user4", "password4")
+    if not res4.succeeded():
+        print("register4: " + res4.get_msg())
+
+    store_res = trading_system.create_store(cookie, "The Store")
+    if not store_res.succeeded():
+        print("open_store: " + store_res.get_msg())
+
+    store_res2 = trading_system.create_store(cookie2, "The Store of cookie2")
+    if not store_res2.succeeded():
+        print("open_store: " + store_res2.get_msg())
+
+    product_res = trading_system.create_product(cookie, store_res.get_obj(), "The Product", "The Category", 5, 8,
+                                                ["The Keyword A", "The Keyword B"])
+    if not product_res.succeeded():
+        print("add_product: " + product_res.get_msg())
+
+    product2_res = trading_system.create_product(cookie, store_res.get_obj(), "The Product2", "The Category2", 10, 16,
+                                                 ["The Keyword A2", "The Keyword B2"])
+    if not product2_res.succeeded():
+        print("add_product: " + product2_res.get_msg())
+
+    res_save_prod = trading_system.save_product_in_cart(cookie2, store_res.get_obj(), product_res.get_obj(), 2)
+    if not res_save_prod.succeeded():
+        print("save in cart: " + res_save_prod.get_msg())
+
+    res_save_prod2 = trading_system.save_product_in_cart(cookie2, store_res.get_obj(), product2_res.get_obj(), 5)
+    if not res_save_prod2.succeeded():
+        print("save in cart: " + res_save_prod.get_msg())
+
+    res_create_offer = trading_system.create_offer(cookie2, store_res.get_obj(), product2_res.get_obj())
+    if not res_create_offer.succeeded():
+        print("create offer: " + res_create_offer.get_msg())
+
+    res_declare_price = trading_system.declare_price(cookie2, res_create_offer.get_obj(), 1.0)
+    if not res_declare_price.succeeded():
+        print("create offer: " + res_declare_price.get_msg())
+
+    res_appoint_owner = trading_system.appoint_owner(cookie, store_res.get_obj(), "user2")
+    if not res_appoint_owner.succeeded():
+        print("appoint owner: " + res_appoint_owner.get_msg())
+
+    res_appoint_owner = trading_system.appoint_owner(cookie2, store_res.get_obj(), "user3")
+    if not res_appoint_owner.succeeded():
+        print("appoint owner: " + res_appoint_owner.get_msg())
+
+    res_appoint_owner = trading_system.appoint_owner(cookie2, store_res2.get_obj(), "user")
+    if not res_appoint_owner.succeeded():
+        print("appoint owner: " + res_appoint_owner.get_msg())
+
+    res_appoint_manager = trading_system.appoint_manager(cookie, store_res2.get_obj(), "user4")
+    if not res_appoint_manager.succeeded():
+        print("appoint manager: " + res_appoint_manager.get_msg())
+
+    res_add_perm = trading_system.add_manager_permission(cookie, store_res2.get_obj(), "user4", "appoint manager")
+    if not res_add_perm.succeeded():
+        print("add permission: " + res_add_perm.get_msg())
+
+    res_remove_perm = trading_system.remove_manager_permission(cookie, store_res2.get_obj(), "user4", "get appointments")
+    if not res_remove_perm.succeeded():
+        print("remove permission: " + res_remove_perm.get_msg())
 
     # trading_system = TradingSystem.getInstance()
-    # cookie2 = trading_system.enter_system()
-    # res = trading_system.get_store("ea936e4b-9752-4559-8999-69e111b81aba")
+    # cookie4 = trading_system.enter_system()
+    # res = trading_system.get_store("2c9cdd79-cfdf-4c5b-9091-dda997dc839b")
     # trading_system.get_store("395eac04-8937-4760-b2b1-228fc2b42ed8")
-    # trading_system.login(cookie2, "user2", "password2")
-    # res = trading_system.appoint_owner(cookie2, "ea936e4b-9752-4559-8999-69e111b81aba", "user3")
+    # trading_system.login(cookie4, "user4", "password4")
+    # res = trading_system.appoint_manager(cookie4, "2c9cdd79-cfdf-4c5b-9091-dda997dc839b", "user3")
     # if not res.succeeded():
     #     print("appoint manager: " + res.get_msg())
 
-
-    trading_system = TradingSystem.getInstance()
-    cookie = trading_system.enter_system()
-    rule_details_complex = {"operator": "or"}
-    rule_details_simple = {"context": {"obj": "product", "identifier": "16"}, "operator": "less-than", "target": 17}
-    rule_type = "simple"
-    parent_id = "1"
-    res = trading_system.login(cookie, "user", "password")
-    res = trading_system.get_store("f8cf6b2c-787d-4c0a-98a8-55176a518032")
-    # res = trading_system.add_purchase_rule(cookie, "f8cf6b2c-787d-4c0a-98a8-55176a518032", rule_details_complex, "complex", parent_id)
-    # res = trading_system.add_purchase_rule(cookie, "f8cf6b2c-787d-4c0a-98a8-55176a518032", rule_details_simple, "simple", "6")
-    # res = trading_system.edit_purchase_rule(cookie, "f8cf6b2c-787d-4c0a-98a8-55176a518032", rule_details_simple, "7", "simple")
-    res = trading_system.edit_purchase_rule(cookie, "f8cf6b2c-787d-4c0a-98a8-55176a518032", rule_details_complex, "6","complex")
-    # res = trading_system.remove_purchase_rule(cookie, "f8cf6b2c-787d-4c0a-98a8-55176a518032", "4")
-
-
-
-    # result_test_clause = trading_system.add_purchase_rule(
-    #     test_clause_details, "simple", test_then_clause_parent_id, clause="test"
-    # )
-    # print(result_test_clause.get_msg())
-
-
-
-    # res = trading_system.add_purchase_rule(cookie, "762f018d-3739-4565-b545-50bcf42b34e2", rule_details, rule_type, parent_id)
+    # trading_system = TradingSystem.getInstance()
+    # cookie = trading_system.enter_system()
+    # rule_details = {"context": {"obj": "product", "identifier": "12"}, "operator": "less-than", "target": 20}
+    # rule_type = "simple"
+    # parent_id = "1"
+    # res = trading_system.login(cookie, "user", "password")
+    # res = trading_system.get_store("1ace3132-d818-429f-8574-7959545187b4")
+    # # res = trading_system.add_purchase_rule(cookie, "1ace3132-d818-429f-8574-7959545187b4", rule_details, rule_type, parent_id)
     # # res = trading_system.remove_purchase_rule(cookie, "4c7d0d66-8c9e-4503-b5cc-78524e6e4c65", "4")
-    # # res = trading_system.edit_purchase_rule(cookie, "762f018d-3739-4565-b545-50bcf42b34e2", rule_details, "9", rule_type)
+    # res = trading_system.edit_purchase_rule(cookie, "1ace3132-d818-429f-8574-7959545187b4", rule_details, "9", rule_type)
     # print(res.get_msg())
 
 
