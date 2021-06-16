5cbb70b496afd4b9a3203a898d8903850c0f7017
Inon Katz <inonk@post.bgu.ac.il>
2021-06-16 18:34:18 +0300

    fix admin bug

--- Backend/DataBase/database.py
+++ Backend/DataBase/database.py
@@ -9,7 +9,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base, scoped_session, regis
 from Backend.response import Response, PrimitiveParsable
 from Backend.settings import Settings
 
-SQLAlchemy_DATABASE_URI = Settings.get_instance(True).get_DB()
+SQLAlchemy_DATABASE_URI = "postgresql://nklfongbputsdr:780ea33e04aa8fb36400cd72967b97385d186978a31d262416b53791ae93c425@ec2-54-155-35-88.eu-west-1.compute.amazonaws.com:5432/d58n31ogjjv4sj"
+SQLAlchemy_DATABASE_URI = Settings.get_instance(False).get_DB()
 engine = create_engine(SQLAlchemy_DATABASE_URI)
 # create a configured "Session" class
 Session = scoped_session(sessionmaker(bind=engine, autocommit=False, autoflush=False))
