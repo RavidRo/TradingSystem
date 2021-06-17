7df59fbd1bd7152d8f365c4407d79239b41376cb
Inon Katz <inonk@post.bgu.ac.il>
2021-06-17 05:55:26 +0300

    maybe fix edit rule bug

--- Backend/Domain/TradingSystem/TypesPolicies/purchase_policy.py
+++ Backend/Domain/TradingSystem/TypesPolicies/purchase_policy.py
@@ -1,3 +1,4 @@
+import copy
 import operator
 import threading
 
@@ -216,6 +217,8 @@ class DefaultPurchasePolicy(PurchasePolicy):
             if leaf_type in leaf_types.keys():
                 edited_rule = leaf_types[leaf_type](self, rule_details, parent)
                 edit_response = self.__purchase_rules.edit_rule(rule_id, edited_rule)
+                parent._children.remove(self)
+                parent._children.append(edited_rule)
                 self.__purchase_rules_lock.release_write()
                 return edit_response
             else:
@@ -230,6 +233,7 @@ class DefaultPurchasePolicy(PurchasePolicy):
             logic_type = rule_details['operator']
             if logic_type in logic_types.keys():
                 edit_response = self.__purchase_rules.edit_rule(rule_id, logic_types[logic_type](parent))
+
                 self.__purchase_rules_lock.release_write()
                 return edit_response
 
